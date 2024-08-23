79dabdddef04918db0644afe45eaabde95efa33a
Eugene <eugeneskom@gmail.com>
2024-08-23 15:21:57 +0300

    made ProductCard a server rendered component

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -9,14 +9,17 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
-  const [isHovered, setIsHovered] = useState(false);
+  // const [isHovered, setIsHovered] = useState(false);
   return (
-    <Link href={`/product/${product.id}-${product.title.toLowerCase().replace(/ /g, "-")}`} className="group flex flex-col h-full bg-white rounded-lg overflow-hidden shadow-md transition-all duration-300 ease-in-out hover:shadow-xl" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+    <Link href={`/product/${product.id}-${product.title.toLowerCase().replace(/ /g, "-")}`} className="group flex flex-col h-full bg-white rounded-lg overflow-hidden shadow-md transition-all duration-300 ease-in-out hover:shadow-xl" 
+    // onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="relative flex-grow">
         <div className="aspect-w-1 aspect-h-1 w-full">
           <Image src={product.thumbnail} alt={product.title} width={370} height={370} style={{ maxWidth: "100%", height: "auto" }} className="w-full h-full object-center object-cover" />
         </div>
-        <div className={`absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center transition-all duration-300 ease-in-out ${isHovered ? "opacity-100" : "opacity-0"} md:opacity-0 md:group-hover:opacity-100 sm:opacity-100 sm:bg-opacity-0 sm:pointer-events-none md:pointer-events-auto`}>
+        <div className={`absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center transition-all duration-300 ease-in-out 
+           md:opacity-0 md:group-hover:opacity-100 sm:opacity-100 sm:bg-opacity-0 sm:pointer-events-none md:pointer-events-auto`}>
           <div className="flex space-x-4">
             <button 
                     type="button"
