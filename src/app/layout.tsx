dacde1810f505dac612ae972826795f457c3724e
Eugene <eugeneskom@gmail.com>
2024-08-23 22:56:07 +0300

    added SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./style.min.css"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <Header />
         {children}
         <Footer />
