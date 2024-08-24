02c8de9b9f00556daf133ba4928ec3a9b684c9d8
Eugene <eugeneskom@gmail.com>
2024-08-24 03:04:06 +0300

    removed inter font from body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body 
+      
+      // className={`${inter.className}`}
+      >
         <SpeedInsights />
         <Header />
         {children}
